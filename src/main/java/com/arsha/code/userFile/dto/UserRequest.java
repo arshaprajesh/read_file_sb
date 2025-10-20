a70fdc56b9d2cd2b4a6981cf74c594853b792945
arsha <arshababu333@gmail.com>
2025-10-20 14:27:05 -0400

    added yaml file

--- src/main/java/com/arsha/code/userFile/dto/UserRequest.java
+++ src/main/java/com/arsha/code/userFile/dto/UserRequest.java
@@ -5,4 +5,36 @@ public class UserRequest {
     public String userName;
     public String userDate;
     public String userState;
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserDate() {
+        return userDate;
+    }
+
+    public void setUserDate(String userDate) {
+        this.userDate = userDate;
+    }
+
+    public String getUserState() {
+        return userState;
+    }
+
+    public void setUserState(String userState) {
+        this.userState = userState;
+    }
 }
